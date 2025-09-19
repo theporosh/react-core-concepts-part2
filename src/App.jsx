90b0092097c5c3677531ffb2e4e7bf499994c990
theporosh <theporosh@gmail.com>
2025-09-19 09:26:36 +0600

    added complete project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
 import Counter from './Counter';
 import Batsman from './Batsman';
+import Users from './Users';
+import Friends from './Friends';
+import Posts from './Posts';
+import Players from './Players';
 import './App.css'
+import { Suspense } from 'react';
+
+
+
+// const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users')
+//    .then(res => res.json())
+
+
+// const fetchFriends = async() => {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/users');
+//     return res.json();
+// }
+
+// const fetchPosts = async() => {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//     return res.json();
+// }
 
 
 function App() {
-  
-  function handleClick(){
+
+  // const postsPromise = fetchPosts();
+
+  // const friendsPromise = fetchFriends();
+
+  function handleClick() {
     alert('I am clicked.')
   }
 
@@ -21,6 +46,21 @@ function App() {
   return (
     <>
       <h3>Vite + React</h3>
+
+      <Players></Players>
+
+      {/* <Suspense fallback={<h4>Posts are coming.......</h4>}>
+        <Posts postsPromise={postsPromise}></Posts>
+      </Suspense> */}
+
+      {/* <Suspense fallback={<h3>Loading...</h3>}>
+        <Users fetchUsers={fetchUsers}></Users>
+      </Suspense> */}
+
+      {/* <Suspense fallback={<h3>Friends are coming for treat...</h3>}> 
+        <Friends friendsPromise={friendsPromise}></Friends>
+      </Suspense> */}
+
       <Batsman></Batsman>
       <Counter></Counter>
 
@@ -29,7 +69,7 @@ function App() {
 
       <button onClick={handleClick}>Click Me</button>
 
-      <button onClick={function handleClick2(){
+      <button onClick={function handleClick2() {
         alert('clicked 2')
       }}>Click Me2</button>
 
@@ -38,7 +78,7 @@ function App() {
       <button onClick={() => alert('Click 4')}>Click Me 4</button>
 
       <button onClick={() => handleAdd5(10)}>Click Add 5</button>
-     
+
     </>
   )
 }
---
+++ src/Summary.jsx
@@ -0,0 +1,45 @@
+/**
+ * Components
+ * JSX
+ * Props
+ * State
+ * Events
+ * [ Conditional Rendering ]
+ * 
+ */
+
+/**
+ * 1. API : URL : https://jsonplaceholder.typicode.com/users 
+ * 
+ * 
+ */
+
+
+/**
+ * 1. just write a simple fetch with json conversion.
+ * 2. wrap the data loading component under suspense 
+ * 
+ * 
+ * */
+
+
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//     .then(res => res.json())
+//     .then(data => console.log(data));
+
+// const loadData = async() => {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/users');
+//     const data = res.json();
+//     return data;
+// }
+
+
+/***
+ * 1. event trigger
+ * 2. state
+ * 3. Data load from API
+ * 4. loop through to display data
+ * 5. why we use key prop
+ * 
+ */ 
